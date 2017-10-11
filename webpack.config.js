62a8818d6b35322fe6dbb9cf0e8bd2f783429dde
Mick Carter <mick.carter@hssnet.com>
2017-10-11 14:50:05 +0100

    Order

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-    entry: ['./index.js', 
-            './node_modules/vue/dist/vue.min.js',
-        './node_modules/ngVue/build/index.js'
+    entry: ['./node_modules/vue/dist/vue.min.js',
+        './node_modules/ngVue/build/index.js',
+        './index.js'
     ],
     output: {
         filename: "dist/vuetastic.js"
